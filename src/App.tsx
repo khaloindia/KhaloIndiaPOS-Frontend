99c7510f43afe431424ee1583550b2d8ca59cdab
khaloindia <khaloindiapos@gmail.com>
2026-09-12 09:51:41 +0530

    Add SuperAdmin route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RestaurantPanel from './pages/RestaurantPanel';
 import CustomerMenu from './pages/CustomerMenu';
+import SuperAdmin from './pages/SuperAdmin';
 
 const LandingPage = () => (
   <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'Segoe UI' }}>
@@ -16,10 +17,10 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/restaurant" element={<RestaurantPanel />} />
         <Route path="/menu" element={<CustomerMenu />} />
+        <Route path="/admin" element={<SuperAdmin />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
