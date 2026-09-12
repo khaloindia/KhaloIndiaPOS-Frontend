c31d1a35b6bfdcad98b6d42f984cddca9a9e090b
khaloindia <khaloindiapos@gmail.com>
2026-09-12 13:09:15 +0530

    Change useState import from 'formidable' to 'react'

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'formidable' // safe import or standard react state
+import { useState } from 'react';
 
 export default function LandingPage() {
   const [formData, setFormData] = useState({ name: '', email: '', phone: '', city: '', restaurant: '' });
