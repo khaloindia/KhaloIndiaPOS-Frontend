f28feec9020f2363ba362c21227c1457c561cda5
khaloindia <khaloindiapos@gmail.com>
2026-09-12 10:13:10 +0530

    Refactor App component by removing LandingPage
    Removed inline LandingPage component definition from App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import RestaurantPanel from './pages/RestaurantPanel';
 import CustomerMenu from './pages/CustomerMenu';
 import SuperAdmin from './pages/SuperAdmin';
-
-const LandingPage = () => (
-  <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'Segoe UI' }}>
-    <h1 style={{ color: '#ff5722' }}>Khalo India POS</h1>
-    <p>Smart Restaurant Management System</p>
-  </div>
-);
 
 function App() {
   return (
